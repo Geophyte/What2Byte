4b002d97ecbf99ce9cb8f50b21de156b7d1bae98
Milan <milanw.2001@gmail.com>
2021-12-12 11:50:54 +0100

    scanned great barcode map ;)

--- scripts/categories.py
+++ scripts/categories.py
@@ -1,34 +1,61 @@
 class WrongCategoryError(Exception):
     def __init__(self):
-        super().__init__("\nWprowadzona kategoria nie istnieje. Wpisz kategorię ponownie.\n")
+        super().__init__(
+            "\nWprowadzona kategoria nie istnieje. Wpisz kategorię ponownie.\n"
+        )
 
 
 class WrongSubcategoryError(Exception):
     def __init__(self):
-        super().__init__("\nWprowadzona podkategoria nie istnieje. Wpisz podkategorię ponownie.\n")
+        super().__init__(
+            "\nWprowadzona podkategoria nie istnieje. Wpisz podkategorię ponownie.\n"
+        )
 
 
-categories = {'do smarowania': ['dżemy i marmolady', 'kremy', 'pasty'],
-              'nabiał': ['jaja i drożdże', 'jogurty i serki', 'mleko i kefir', 'produkty twarogowe', 'sery', 'śmietany'],
-              'napoje': None,
-              'owoce i orzechy': None,
-              'pieczywo': ['bułki', 'chleby', 'tortille', 'zamienniki pieczywa'],
-              'produkty gotowe': None,
-              'produkty mięsne': ['konserwy i pasztety', 'ryby i owoce morza', 'świeże mięsa', 'wędliny i mięsa w plastrach'],
-              'przyprawy i dodatki w proszku': None,
-              'sosy i dodatki': ['ketchup, majonez, musztarda', 'Sosy'],
-              'spiżarnia': ['do słodzenia', 'kasze i ryż', 'kawa, kakao, herbata', 'makarony', 'mąki', 'ocet', 'produkty zbożowe', 'puszki i słoiki'],
-              'tłuszcze': None,
-              'warzywa i grzyby': None}
+categories = {
+    "do smarowania": ["dżemy i marmolady", "kremy", "pasty"],
+    "nabiał": [
+        "jaja i drożdże",
+        "jogurty i serki",
+        "mleko i kefir",
+        "produkty twarogowe",
+        "sery",
+        "śmietany",
+    ],
+    "napoje": None,
+    "owoce i orzechy": None,
+    "pieczywo": ["bułki", "chleby", "tortille", "zamienniki pieczywa"],
+    "produkty gotowe": None,
+    "produkty mięsne": [
+        "konserwy i pasztety",
+        "ryby i owoce morza",
+        "świeże mięsa",
+        "wędliny i mięsa w plastrach",
+    ],
+    "przyprawy i dodatki w proszku": None,
+    "sosy i dodatki": ["ketchup, majonez, musztarda", "Sosy"],
+    "spiżarnia": [
+        "do słodzenia",
+        "kasze i ryż",
+        "kawa, kakao, herbata",
+        "makarony",
+        "mąki",
+        "ocet",
+        "produkty zbożowe",
+        "puszki i słoiki",
+    ],
+    "tłuszcze": None,
+    "warzywa i grzyby": None,
+}
 
 
-def product_category(categories: dict) -> str:
+def product_category(categories: dict = categories) -> str:
     """Zwraca nazwę (pod)kategorii wybranej ze słownika"""
 
     print("Wybierz kategorię: ")
     for category in categories:
         print(category)
-    choice1 = input('\n')
+    choice1 = input("\n")
     if choice1.lower() not in categories.keys():
         raise WrongCategoryError
     if not categories[choice1.lower()]:
@@ -38,7 +65,7 @@ def product_category(categories: dict) -> str:
         print("\nWybierz podkategorię: ")
         for subcategory in subcategories:
             print(subcategory)
-        choice2 = input('\n')
+        choice2 = input("\n")
         if choice2.lower() not in subcategories:
             raise WrongSubcategoryError
         else:
